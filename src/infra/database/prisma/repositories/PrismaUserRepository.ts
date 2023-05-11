cb87ae146a1864333b9a3f89390353e75350bb66
Jackson Silva Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-05-11 17:18:01 -0300

    feat: create prisma repository

--- src/infra/database/prisma/repositories/PrismaUserRepository.ts
+++ src/infra/database/prisma/repositories/PrismaUserRepository.ts
@@ -2,10 +2,10 @@ import { User } from "~/domain/entities/User";
 import { UserRepository } from "~/domain/repositories/UserRepository";
 
 export class PrismaUserRepository implements UserRepository {
-  findById(id: string): Promise<User | null> {
+  async findById(id: string): Promise<User | null> {
     throw new Error("Method not implemented.");
   }
-  create(user: User): Promise<void> {
+  async create(user: User): Promise<void> {
     throw new Error("Method not implemented.");
   }
 }
