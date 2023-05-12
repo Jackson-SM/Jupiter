c419bcd4718121d72101b7e4d21b3cdc741630ae
Jackson Silva Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-05-12 13:56:55 -0300

    fix: fixed types email case

--- src/application/use-cases/users/find-user-by-email.ts
+++ src/application/use-cases/users/find-user-by-email.ts
@@ -2,21 +2,23 @@ import { UserRepository } from "~/domain/repositories/UserRepository";
 import { UserNotFound } from "./errors/UserNotFound";
 import { User } from "~/domain/entities/User/User";
 
-interface FindUserByIdRequest {
+interface FindUserByEmailRequest {
   email: string;
 }
 
-interface FindUserByIdResponse {
+interface FindUserByEmailResponse {
   user: User;
 }
 
 export class FindUserByIdCase {
   constructor(private userRepository: UserRepository) {}
 
-  async execute(request: FindUserByIdRequest): Promise<FindUserByIdResponse> {
+  async execute(
+    request: FindUserByEmailRequest,
+  ): Promise<FindUserByEmailResponse> {
     const { email } = request;
 
-    const user = await this.userRepository.findById(email);
+    const user = await this.userRepository.findByEmail(email);
 
     if (!user) {
       throw new UserNotFound();
