d1cb342134b4f5246e76a647b15ce16803b682ab
Jackson Silva Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-05-13 17:45:33 -0300

    fix: fixed code repeat in testing

--- src/application/use-cases/users/find-user-by-id-case.spec.ts
+++ src/application/use-cases/users/find-user-by-id-case.spec.ts
@@ -3,26 +3,30 @@ import { CreateUserCase } from "./create-user-case";
 import { FindUserByIdCase } from "./find-user-by-id-case";
 import { makeUser } from "tests/factories/makeUser";
 import { UserNotFound } from "./errors/UserNotFound";
+import { User } from "~/domain/entities/User/User";
 
 describe("Find By Id User Case", () => {
-  it("should create a new user", async () => {
-    const inMemoryRepository = new InMemoryUserRepository();
-    const findUserById = new FindUserByIdCase(inMemoryRepository);
-    const createUserCase = new CreateUserCase(inMemoryRepository);
+  let inMemoryRepository: InMemoryUserRepository;
+  let findUserById: FindUserByIdCase;
+  let createUserCase: CreateUserCase;
+  let userTesting: User;
 
-    const userCreated = await createUserCase.execute(makeUser());
-
-    const { user } = await findUserById.execute({ id: userCreated.user.id });
-
-    expect(inMemoryRepository.users[0]).toEqual(user);
+  beforeEach(async () => {
+    inMemoryRepository = new InMemoryUserRepository();
+    findUserById = new FindUserByIdCase(inMemoryRepository);
+    createUserCase = new CreateUserCase(inMemoryRepository);
+    const { user } = await createUserCase.execute(makeUser());
+    userTesting = user;
   });
-  it("should return new error case users void", async () => {
-    const inMemoryRepository = new InMemoryUserRepository();
-    const findUserById = new FindUserByIdCase(inMemoryRepository);
-    const createUserCase = new CreateUserCase(inMemoryRepository);
 
-    await createUserCase.execute(makeUser());
+  it("should return a user in first position", async () => {
+    const { user: raw } = await findUserById.execute({
+      id: userTesting.id,
+    });
 
+    expect(raw).toEqual(userTesting);
+  });
+  it("should return new error case user not exists", async () => {
     await expect(findUserById.execute({ id: "a".repeat(20) })).rejects.toThrow(
       UserNotFound,
     );
