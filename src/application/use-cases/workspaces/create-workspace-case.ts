53af2bc7a248271b50bb2770d91377413bd5842f
Jackson Silva Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-05-13 11:22:32 -0300

    feat: create workspace case

--- src/application/use-cases/workspaces/create-workspace-case.ts
+++ src/application/use-cases/workspaces/create-workspace-case.ts
@@ -1,10 +1,30 @@
 import { Workspace } from "../../../domain/entities/Workspaces/Workspace";
 import { WorkspaceRepository } from "~/domain/repositories/WorkspaceRepository";
 
+interface CreateWorkspaceRequest {
+  title: string;
+  description: string;
+  creatorId: string;
+}
+
+interface CreateWorkspaceResponse {
+  workspace: Workspace;
+}
+
 export class CreateWorkspaceCase {
   constructor(private workspaceRepository: WorkspaceRepository) {}
 
-  async execute(workspace: Workspace): Promise<void> {
-    const user = await this.workspaceRepository.create(workspace);
+  async execute(
+    createWorkspaceRequest: CreateWorkspaceRequest,
+  ): Promise<CreateWorkspaceResponse> {
+    const { title, description, creatorId } = createWorkspaceRequest;
+
+    const workspace = new Workspace({ title, description, creatorId });
+
+    await this.workspaceRepository.create(workspace);
+
+    return {
+      workspace,
+    };
   }
 }
