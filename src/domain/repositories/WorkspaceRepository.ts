56e96b3025396ec9d5946ab336410ec1bf6a985c
Jackson Silva Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-05-13 11:57:30 -0300

    feat: add in workspace repository findById

--- src/domain/repositories/WorkspaceRepository.ts
+++ src/domain/repositories/WorkspaceRepository.ts
@@ -2,4 +2,5 @@ import { Workspace } from "../entities/Workspaces/Workspace";
 
 export interface WorkspaceRepository {
   create(workspace: Workspace): Promise<void>;
+  findByid(id: string): Promise<Workspace>;
 }
