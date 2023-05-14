c3d5bb8447481b8cb250e68ec5814bebb6442bcc
Jackson Silva Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-05-14 19:48:56 -0300

    feat: find by email add

--- src/domain/repositories/UserRepository.ts
+++ src/domain/repositories/UserRepository.ts
@@ -3,4 +3,5 @@ import { User } from "../entities/User/User";
 export interface UserRepository {
   create(user: User): Promise<void>;
   findById(id: string): Promise<User | null>;
+  findByEmail(email: string): Promise<User | null>;
 }
