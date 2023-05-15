ab39a857611b5cce697af7130c5d74bd7310ae0d
Jackson Silva Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-05-15 16:07:07 -0300

    feat: add create workspace controller function

--- src/infra/http/controllers/Workspace/CreateWorkspaceController.ts
+++ src/infra/http/controllers/Workspace/CreateWorkspaceController.ts
@@ -1,7 +1,29 @@
+import { CreateWorkspaceCase } from "../../../../application/use-cases/workspaces/create-workspace-case";
+import { PrismaWorkspaceRepository } from "../../../../infra/database/prisma/repositories/prisma-workspace-repository";
+import { ICreateWorkspaceBody } from "../../dtos/create-workspace-body";
+import { JwtPayload } from "../../../../application/services/AuthService";
 import { Request, ResponseToolkit } from "@hapi/hapi";
+import Boom from "@hapi/boom";
 
 export class CreateWorkspaceController {
+  constructor(private createWorkspaceCase: CreateWorkspaceCase) {}
+
   async handler(request: Request, h: ResponseToolkit) {
-    return "Create Workspace";
+    const { title, description } = request.payload as ICreateWorkspaceBody;
+    const { id } = request.auth.credentials;
+
+    const workspace = await createWorkspaceCase.execute({
+      title,
+      description,
+      creatorId: String(id),
+    });
+
+    return h.response(workspace).code(201);
   }
 }
+
+const prismaRepository = new PrismaWorkspaceRepository();
+const createWorkspaceCase = new CreateWorkspaceCase(prismaRepository);
+export const createWorkspaceController = new CreateWorkspaceController(
+  createWorkspaceCase,
+);
