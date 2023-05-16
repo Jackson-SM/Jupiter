e58f0710765f4b19120898411a3488b69b107c76
Jackson Silva Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-05-15 21:23:41 -0300

    refactor: refactoring code of controllers

--- src/infra/http/controllers/Workspace/CreateWorkspaceController.ts
+++ src/infra/http/controllers/Workspace/CreateWorkspaceController.ts
@@ -1,9 +1,7 @@
 import { CreateWorkspaceCase } from "../../../../application/use-cases/workspaces/create-workspace-case";
-import { PrismaWorkspaceRepository } from "../../../../infra/database/prisma/repositories/prisma-workspace-repository";
 import { ICreateWorkspaceBody } from "../../dtos/create-workspace-body";
-import { JwtPayload } from "../../../../application/services/AuthService";
 import { Request, ResponseToolkit } from "@hapi/hapi";
-import Boom from "@hapi/boom";
+import { WorkspaceViewModel } from "../../view-models/workspace-view-model";
 
 export class CreateWorkspaceController {
   constructor(private createWorkspaceCase: CreateWorkspaceCase) {}
@@ -12,18 +10,12 @@ export class CreateWorkspaceController {
     const { title, description } = request.payload as ICreateWorkspaceBody;
     const { id } = request.auth.credentials;
 
-    const workspace = await createWorkspaceCase.execute({
+    const { workspace } = await this.createWorkspaceCase.execute({
       title,
       description,
       creatorId: String(id),
     });
 
-    return h.response(workspace).code(201);
+    return h.response(WorkspaceViewModel.toHttp(workspace)).code(201);
   }
 }
-
-const prismaRepository = new PrismaWorkspaceRepository();
-const createWorkspaceCase = new CreateWorkspaceCase(prismaRepository);
-export const createWorkspaceController = new CreateWorkspaceController(
-  createWorkspaceCase,
-);
---
+++ src/infra/http/controllers/Workspace/FindWorkspaceByIdController.ts
@@ -0,0 +1,22 @@
+import { Request, ResponseToolkit } from "@hapi/hapi";
+import { WorkspaceRepository } from "../../../../domain/repositories/WorkspaceRepository";
+import { FindWorkspaceByIdCase } from "~/application/use-cases/workspaces/find-workspace-by-id-case";
+import { WorkspaceViewModel } from "../../view-models/workspace-view-model";
+
+export class FindWorkspaceByIdController {
+  constructor(private findWorkspaceByIdCase: FindWorkspaceByIdCase) {}
+
+  async handler(request: Request, h: ResponseToolkit) {
+    const { id } = request.params as { id: string };
+
+    try {
+      const { workspace } = await this.findWorkspaceByIdCase.execute({
+        id: id,
+      });
+
+      return WorkspaceViewModel.toHttp(workspace);
+    } catch (err: any) {
+      return h.response({ message: err.message }).code(err.statusCode);
+    }
+  }
+}
