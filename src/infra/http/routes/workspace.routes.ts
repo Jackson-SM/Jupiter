0888ca7940dae77842a2f97fbd755901c7a7629b
Jackson Silva Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-05-16 11:59:42 -0300

    feat: add route get workspace by id

--- src/infra/http/routes/workspace.routes.ts
+++ src/infra/http/routes/workspace.routes.ts
@@ -1,10 +1,18 @@
 import Hapi from "@hapi/hapi";
-import { createWorkspaceController } from "../controllers/Workspace";
+import {
+  createWorkspaceController,
+  findWorkspaceByIdController,
+} from "../controllers/Workspace";
 
 const workspacesRoutes = {
   name: "workspaces",
   register: async function (server: Hapi.Server) {
     server.route([
+      {
+        method: "GET",
+        path: "/v1/workspaces",
+        handler: findWorkspaceByIdController.handler,
+      },
       {
         method: "POST",
         path: "/v1/workspaces",
