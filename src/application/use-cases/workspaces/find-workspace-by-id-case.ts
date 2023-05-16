26666bd1adfe6c8499760692a86ea9acba6ef1e6
Jackson Silva Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-05-15 21:24:49 -0300

    fix: create types of return and request

--- src/application/use-cases/workspaces/find-workspace-by-id-case.ts
+++ src/application/use-cases/workspaces/find-workspace-by-id-case.ts
@@ -2,16 +2,28 @@ import { Workspace } from "../../../domain/entities/Workspaces/Workspace";
 import { WorkspaceRepository } from "../../../domain/repositories/WorkspaceRepository";
 import { WorkspaceNotFound } from "./errors/WorkspaceNotFound";
 
+interface FindWorkspaceRequest {
+  id: string;
+}
+
+interface FindWorkspaceResponse {
+  workspace: Workspace;
+}
+
 export class FindWorkspaceByIdCase {
   constructor(private workspaceRepository: WorkspaceRepository) {}
 
-  async execute(id: string): Promise<Workspace> {
+  async execute(request: FindWorkspaceRequest): Promise<FindWorkspaceResponse> {
+    const { id } = request;
+
     const workspace = await this.workspaceRepository.findByid(id);
 
     if (!workspace) {
       throw new WorkspaceNotFound();
     }
 
-    return workspace;
+    return {
+      workspace,
+    };
   }
 }
