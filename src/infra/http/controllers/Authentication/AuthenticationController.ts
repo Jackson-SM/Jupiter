eab64067e597a7a859ce85149666f70ba04c60dc
Jackson Silva Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-05-16 23:38:06 -0300

    feat: add authentication controller

---
+++ src/infra/http/controllers/Authentication/AuthenticationController.ts
@@ -0,0 +1,22 @@
+import { Request, ResponseToolkit } from "@hapi/hapi";
+import { AuthenticationUseCase } from "~/application/use-cases/auths/authentication-use-case";
+import { IAuthenticateBody } from "../../dtos/authenticate-body";
+
+export class AuthenticationController {
+  constructor(private authencationUseCase: AuthenticationUseCase) {}
+
+  public handler = async (request: Request, h: ResponseToolkit) => {
+    const { email, password } = request.payload as IAuthenticateBody;
+
+    try {
+      const token = await this.authencationUseCase.authenticate(
+        email,
+        password,
+      );
+
+      return token;
+    } catch (err: any) {
+      h.response(err).code(err.statusCode || 500);
+    }
+  };
+}
