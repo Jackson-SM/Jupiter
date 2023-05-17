14e7e02e2fb4ccad2467375402a64a3e2463a3ae
Jackson Silva Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-05-17 00:10:48 -0300

    feat: add route authenticate login

---
+++ src/infra/http/routes/auth.routes.ts
@@ -0,0 +1,20 @@
+import Hapi, { Request, ResponseToolkit } from "@hapi/hapi";
+import { authenticationLoginController } from "../controllers/Authentication";
+
+const authRoutes = {
+  name: "auths",
+  register: async function (server: Hapi.Server) {
+    server.route([
+      {
+        method: "POST",
+        path: "/v1/login",
+        handler: authenticationLoginController.handler,
+        options: {
+          auth: false,
+        },
+      },
+    ]);
+  },
+};
+
+export default authRoutes;
