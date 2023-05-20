ee90fdcecb7c4fab17e1bd1d3007250d9aa57df4
Jackson Silva Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-05-20 17:03:15 -0300

    fix: project repository

--- src/domain/repositories/ProjectRepository.ts
+++ src/domain/repositories/ProjectRepository.ts
@@ -2,7 +2,7 @@ import { Project } from "../entities/Project/Project";
 
 export interface ProjectRepository {
   create(project: Project): Promise<void>;
-  findByid(id: string): Promise<Project | null>;
-  findByLeadId(leadId: string): Promise<Project | null>;
-  findByWorkspaceId(workspaceId: string): Promise<Project | null>;
+  findById(id: string): Promise<Project | null>;
+  findAllByLeadId(leadId: string): Promise<Project[]>;
+  findAllByWorkspaceId(workspaceId: string): Promise<Project[]>;
 }
