e0b84c5daa728234907bbbbfa3a154444be85d80
Jackson Silva Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-05-20 17:56:31 -0300

    fix: fixed function in repository comments

--- src/domain/repositories/CommentRepository.ts
+++ src/domain/repositories/CommentRepository.ts
@@ -3,7 +3,6 @@ import { Comment } from "../entities/Comment/Comment";
 export interface CommentRepository {
   create(task: Comment): Promise<void>;
   findByid(id: string): Promise<Comment | null>;
-  findByUserId(userId: string): Promise<Comment | null>;
+  findAllByUserId(userId: string): Promise<Comment[]>;
   findAllCommentsByTaskId(taskId: string): Promise<Comment[]>;
-  findAllCommentsByUser(userId: string): Promise<Comment[]>;
 }
