e369c5f59bf69e1f569798611a4f864c0f0f6011
Jackson Silva Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-05-21 03:35:22 -0300

    test: find task by responsible id case testing

---
+++ src/application/use-cases/tasks/find-task-by-reponsible-id-case.spec.ts
@@ -0,0 +1,48 @@
+import { CreateTaskCase } from "./create-task-case";
+import { InMemoryTaskRepository } from "tests/repositories/in-memory-task-repository";
+import { makeTask } from "tests/factories/makeTask";
+import { Task } from "~/domain/entities/Task/Task";
+import { FindTaskByProjectIdCase } from "./find-task-by-project-id-case";
+import { Project } from "~/domain/entities/Project/Project";
+import { makeProject } from "tests/factories/makeProject";
+import { User } from "~/domain/entities/User/User";
+import { makeUser } from "tests/factories/makeUser";
+import { AddTaskResponsibleCase } from "./add-task-responsible-case";
+import { FindTaskByResponsibleIdCase } from "./find-task-by-responsible-id-case";
+
+describe("Find Task By Responsible Id Case", () => {
+  let inMemoryTaskRepository: InMemoryTaskRepository;
+  let createTaskCase: CreateTaskCase;
+  let findByResponsibleIdCase: FindTaskByResponsibleIdCase;
+  let addTaskResponsibleCase: AddTaskResponsibleCase;
+  let taskTesting: Task;
+  let userTesting: User;
+
+  beforeEach(async () => {
+    inMemoryTaskRepository = new InMemoryTaskRepository();
+    createTaskCase = new CreateTaskCase(inMemoryTaskRepository);
+    addTaskResponsibleCase = new AddTaskResponsibleCase(inMemoryTaskRepository);
+    findByResponsibleIdCase = new FindTaskByResponsibleIdCase(
+      inMemoryTaskRepository,
+    );
+    userTesting = makeUser();
+    const { task } = await createTaskCase.execute(
+      makeTask({ projectId: makeProject({ leadId: userTesting.id }).id }),
+    );
+    taskTesting = task;
+    await addTaskResponsibleCase.execute({
+      userId: userTesting.id,
+      taskId: taskTesting.id,
+    });
+  });
+
+  it("should return tasks user is responsible", async () => {
+    const { tasks } = await findByResponsibleIdCase.execute({
+      id: userTesting.id,
+    });
+
+    expect(tasks).toEqual(
+      expect.arrayContaining([expect.objectContaining(taskTesting)]),
+    );
+  });
+});
