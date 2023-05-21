ea6e51d1bf3bfa49a9c327003c78b9861f01e8dc
Jackson Silva Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-05-21 00:44:43 -0300

    feat: add relation between entities

--- tests/repositories/in-memory-task-repository.ts
+++ tests/repositories/in-memory-task-repository.ts
@@ -1,9 +1,11 @@
 import { Project } from "~/domain/entities/Project/Project";
 import { Task } from "~/domain/entities/Task/Task";
+import { TaskResponsible } from "~/domain/entities/TaskReponsible/TaskReponsible";
 import { TaskRepository } from "~/domain/repositories/TaskRepository";
 
 export class InMemoryTaskRepository implements TaskRepository {
   public tasks: Task[] = [];
+  public taskResponsible: TaskResponsible[];
 
   async findByid(id: string): Promise<Task | null> {
     const task = this.tasks.find((task) => task.id === id);
@@ -15,26 +17,21 @@ export class InMemoryTaskRepository implements TaskRepository {
     return task;
   }
 
-  async findByProjectId(projectId: string): Promise<Task | null> {
-    const task = this.tasks.find((task) => task.projectId === projectId);
-
-    if (!task) {
-      return null;
-    }
+  async findByProjectId(projectId: string): Promise<Task[]> {
+    const task = this.tasks.filter((task) => task.projectId === projectId);
 
     return task;
   }
 
-  async findByResponsibleId(responsibleId: string): Promise<Task | null> {
-    const task = this.tasks.find(
-      (task) => task.responsibleId === responsibleId,
+  async findByResponsibleId(responsibleId: string): Promise<Task[]> {
+    const taskRelation = this.taskResponsible
+      .filter((task) => task.userId === responsibleId)
+      .map((task) => task.taskId);
+
+    const tasksResponsible = this.tasks.filter((task) =>
+      taskRelation.includes(task.id),
     );
-
-    if (!task) {
-      return null;
-    }
-
-    return task;
+    return tasksResponsible;
   }
   async create(task: Task): Promise<void> {
     this.tasks.push(task);
