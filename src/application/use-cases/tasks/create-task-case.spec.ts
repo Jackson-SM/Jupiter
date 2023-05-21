0db1c9f28683ac32f060d8bdb3eadbbe2434d485
Jackson Silva Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-05-21 03:34:18 -0300

    test: testing create task case

---
+++ src/application/use-cases/tasks/create-task-case.spec.ts
@@ -0,0 +1,27 @@
+import { InMemoryTaskRepository } from "tests/repositories/in-memory-task-repository";
+import { CreateTaskCase } from "./create-task-case";
+import { makeTask } from "tests/factories/makeTask";
+import { Task } from "~/domain/entities/Task/Task";
+import { FindTaskByIdCase } from "./find-task-by-id-case";
+
+describe("Create Task Case", () => {
+  let inMemoryTaskRepository: InMemoryTaskRepository;
+  let createTaskCase: CreateTaskCase;
+  let findTaskByIdCase: FindTaskByIdCase;
+  let taskTesting: Task;
+
+  beforeEach(async () => {
+    inMemoryTaskRepository = new InMemoryTaskRepository();
+    createTaskCase = new CreateTaskCase(inMemoryTaskRepository);
+    findTaskByIdCase = new FindTaskByIdCase(inMemoryTaskRepository);
+    const { task } = await createTaskCase.execute(makeTask());
+    taskTesting = task;
+  });
+
+  it("should create a new task", async () => {
+    const { task } = await findTaskByIdCase.execute({ id: taskTesting.id });
+
+    expect(task).toBeDefined();
+    expect(task).toEqual(taskTesting);
+  });
+});
