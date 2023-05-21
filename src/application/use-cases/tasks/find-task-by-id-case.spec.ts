29a9260fc5143842c06f1b7ed830601e9e29c61e
Jackson Silva Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-05-21 03:34:38 -0300

    test: testing find task by id case

---
+++ src/application/use-cases/tasks/find-task-by-id-case.spec.ts
@@ -0,0 +1,36 @@
+import { CreateTaskCase } from "./create-task-case";
+import { FindTaskByIdCase } from "./find-task-by-id-case";
+import { InMemoryTaskRepository } from "tests/repositories/in-memory-task-repository";
+import { makeTask } from "tests/factories/makeTask";
+import { Task } from "~/domain/entities/Task/Task";
+
+describe("Find Task By Id Case", () => {
+  let inMemoryTaskRepository: InMemoryTaskRepository;
+  let createTaskCase: CreateTaskCase;
+  let findTaskByIdCase: FindTaskByIdCase;
+  let taskTesting: Task;
+
+  beforeEach(async () => {
+    inMemoryTaskRepository = new InMemoryTaskRepository();
+    createTaskCase = new CreateTaskCase(inMemoryTaskRepository);
+    findTaskByIdCase = new FindTaskByIdCase(inMemoryTaskRepository);
+    const { task } = await createTaskCase.execute(makeTask());
+    taskTesting = task;
+  });
+
+  it("should to find a new Task was created", async () => {
+    const { task } = await findTaskByIdCase.execute({
+      id: taskTesting.id,
+    });
+
+    expect(task).toEqual(taskTesting);
+    expect(task).toBeDefined();
+  });
+  it("should throw new error if Task not exists", async () => {
+    await expect(
+      findTaskByIdCase.execute({
+        id: makeTask().id,
+      }),
+    ).rejects.toThrow();
+  });
+});
