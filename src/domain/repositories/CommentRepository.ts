1675b13b7fe50a8ad7b226a545462ad6f5494f5a
Jackson Silva Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-05-20 22:22:45 -0300

    fix: fixed name argument task for comment

--- src/domain/repositories/CommentRepository.ts
+++ src/domain/repositories/CommentRepository.ts
@@ -1,7 +1,7 @@
 import { Comment } from "../entities/Comment/Comment";
 
 export interface CommentRepository {
-  create(task: Comment): Promise<void>;
+  create(comment: Comment): Promise<void>;
   findByid(id: string): Promise<Comment | null>;
   findAllByUserId(userId: string): Promise<Comment[]>;
   findAllCommentsByTaskId(taskId: string): Promise<Comment[]>;
