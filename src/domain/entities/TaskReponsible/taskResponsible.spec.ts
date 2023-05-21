029d068926562827613ccf1ffb90539658e9ae34
Jackson Silva Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-05-21 00:12:22 -0300

    test: add task responsible entity

---
+++ src/domain/entities/TaskReponsible/taskResponsible.spec.ts
@@ -0,0 +1,14 @@
+import { makeUser } from "tests/factories/makeUser";
+import { TaskResponsible } from "./TaskReponsible";
+
+describe("Task Responsible", () => {
+  it("should create a new task responsible instance", () => {
+    const taskResponsible = new TaskResponsible({
+      taskId: "taskId",
+      userId: "userId",
+    });
+
+    expect(taskResponsible).toBeTruthy();
+    expect(taskResponsible).toBeDefined();
+  });
+});
