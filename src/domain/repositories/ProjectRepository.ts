a6ef9dd26be79e414a037d1fb6541b1fbf5544d4
Jackson Silva Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-05-22 00:23:41 -0300

    feat: add participants relation

--- src/domain/repositories/ProjectRepository.ts
+++ src/domain/repositories/ProjectRepository.ts
@@ -1,8 +1,11 @@
 import { Project } from "../entities/Project/Project";
+import { User } from "../entities/User/User";
 
 export interface ProjectRepository {
   create(project: Project): Promise<void>;
   findById(id: string): Promise<Project | null>;
   findAllByLeadId(leadId: string): Promise<Project[]>;
   findAllByWorkspaceId(workspaceId: string): Promise<Project[]>;
+  findAllParticipantsByProjectId(projectId: string): Promise<User[]>;
+  addParticipantsInProject(userId: string, projectId: string): Promise<void>;
 }
--- tests/repositories/in-memory-project-repository.ts
+++ tests/repositories/in-memory-project-repository.ts
@@ -1,8 +1,14 @@
 import { Project } from "~/domain/entities/Project/Project";
+import { ProjectParticipanting } from "~/domain/entities/ProjectParticipanting/ProjectParticipanting";
+import { User } from "~/domain/entities/User/User";
 import { ProjectRepository } from "~/domain/repositories/ProjectRepository";
+import { UserRepository } from "~/domain/repositories/UserRepository";
 
 export class InMemoryProjectRepository implements ProjectRepository {
   public projects: Project[] = [];
+  public participants: ProjectParticipanting[] = [];
+
+  constructor(private userRepository?: UserRepository) {}
 
   async findById(id: string): Promise<Project | null> {
     const project = await this.projects.find((project) => project.id === id);
@@ -28,6 +34,29 @@ export class InMemoryProjectRepository implements ProjectRepository {
     );
 
     return project;
+  }
+
+  async addParticipantsInProject(
+    userId: string,
+    projectId: string,
+  ): Promise<void> {
+    const projectParticipant = new ProjectParticipanting({ userId, projectId });
+
+    this.participants.push(projectParticipant);
+  }
+
+  async findAllParticipantsByProjectId(projectId: string): Promise<User[]> {
+    const userIdsParticipantingProject = await this.participants
+      .filter((project) => project.projectId === projectId)
+      .map((project) => project.userId);
+
+    const filterUsersParticipants = userIdsParticipantingProject.map((userId) =>
+      this.userRepository?.findById(userId),
+    );
+
+    const users = await Promise.all(filterUsersParticipants);
+
+    return users.filter((user) => user !== null) as User[];
   }
   async create(project: Project): Promise<void> {
     this.projects.push(project);
