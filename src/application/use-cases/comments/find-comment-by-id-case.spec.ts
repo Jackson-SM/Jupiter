4375be70428beeeac81972688b733bf36ca06aca
Jackson Silva Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-05-22 02:20:02 -0300

    test: find comment by id case testing

---
+++ src/application/use-cases/comments/find-comment-by-id-case.spec.ts
@@ -0,0 +1,35 @@
+import { InMemoryCommentRepository } from "tests/repositories/in-memory-comment-repository";
+import { CreateCommentCase } from "./create-comment-case";
+import { makeComment } from "tests/factories/makeComment";
+import { FindCommentByIdCase } from "./find-comment-by-id-case";
+import { Comment } from "~/domain/entities/Comment/Comment";
+
+describe("Find Comment  Case", () => {
+  let inMemoryCommentRepository: InMemoryCommentRepository;
+  let createCommentCase: CreateCommentCase;
+  let findCommentByIdCase: FindCommentByIdCase;
+  let commentTesting: Comment;
+
+  beforeEach(async () => {
+    inMemoryCommentRepository = new InMemoryCommentRepository();
+    createCommentCase = new CreateCommentCase(inMemoryCommentRepository);
+    findCommentByIdCase = new FindCommentByIdCase(inMemoryCommentRepository);
+    const { comment } = await createCommentCase.execute(makeComment());
+    commentTesting = comment;
+  });
+
+  it("should find a comment in comments array", async () => {
+    const { comment: commentFound } = await findCommentByIdCase.execute({
+      id: commentTesting.id,
+    });
+    expect(commentFound).toEqual(commentTesting);
+    expect(commentFound).toBeInstanceOf(Comment);
+  });
+  it("should throw error if comment not found", async () => {
+    await expect(
+      findCommentByIdCase.execute({
+        id: "idtesting",
+      }),
+    ).rejects.toThrow();
+  });
+});
