c520d5cd45afa3bc1a3e4b4c57eb2674ace4d1f7
Jackson Silva Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-05-22 12:22:04 -0300

    feat: add indexjs for inject dependency

---
+++ src/infra/http/controllers/Project/index.ts
@@ -0,0 +1,56 @@
+import { PrismaProjectRepository } from "~/infra/database/prisma/repositories/prisma-project-repository";
+import { CreateProjectController } from "./CreateProjectController";
+import { CreateProjectCase } from "~/application/use-cases/projects/create-project-case";
+import { FindProjectByIdController } from "./FindProjectByIdController";
+import { FindProjectByIdCase } from "~/application/use-cases/projects/find-project-by-id-case";
+import { FindProjectByLeadIdController } from "./FindProjectByLeadIdController";
+import { FindProjectByWorkspaceIdController } from "./FindProjectByWorkspaceIdController";
+import { FindAllParticipantsByProjectIdController } from "./FindAllParticipantsByProjectIdController";
+import { FindProjectByLeadIdCase } from "~/application/use-cases/projects/find-project-by-lead-id-case";
+import { FindProjectByWorkspaceIdCase } from "~/application/use-cases/projects/find-project-by-workspace-id-case";
+import { FindAllParticipantsByProjectIdCase } from "~/application/use-cases/projects/find-all-participants-by-project-id-case";
+import { AddParticipantInProjectController } from "./AddParticipantInProjectController";
+import { AddParticipantInProjectCase } from "~/application/use-cases/projects/add-participant-in-project-case";
+
+const prismaProjectRepository = new PrismaProjectRepository();
+
+//Use Cases
+const createProjectCase = new CreateProjectCase(prismaProjectRepository);
+const findProjectByIdCase = new FindProjectByIdCase(prismaProjectRepository);
+const findProjectByLeadIdCase = new FindProjectByLeadIdCase(
+  prismaProjectRepository,
+);
+const findProjectByWorkspaceIdCase = new FindProjectByWorkspaceIdCase(
+  prismaProjectRepository,
+);
+const findAllParticipantsByProjectIdCase =
+  new FindAllParticipantsByProjectIdCase(prismaProjectRepository);
+const addParticipantInProjectCase = new AddParticipantInProjectCase(
+  prismaProjectRepository,
+);
+// Controllers
+const createProjectController = new CreateProjectController(createProjectCase);
+const findProjectByIdController = new FindProjectByIdController(
+  findProjectByIdCase,
+);
+const findProjectByLeadIdController = new FindProjectByLeadIdController(
+  findProjectByLeadIdCase,
+);
+const findProjectByWorkspaceIdIdController =
+  new FindProjectByWorkspaceIdController(findProjectByWorkspaceIdCase);
+const findAllParticipantsByProjectIdController =
+  new FindAllParticipantsByProjectIdController(
+    findAllParticipantsByProjectIdCase,
+  );
+const addParticipantInProjectController = new AddParticipantInProjectController(
+  addParticipantInProjectCase,
+);
+
+export {
+  createProjectController,
+  findProjectByIdController,
+  findProjectByLeadIdController,
+  findProjectByWorkspaceIdIdController,
+  findAllParticipantsByProjectIdController,
+  addParticipantInProjectController,
+};
