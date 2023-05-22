91fc1c97fa78203ba1f97941ac0ae2b74a540e40
Jackson Silva Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-05-22 00:51:25 -0300

    feat: add find all participants and add participants em prisma repository

--- src/infra/database/prisma/repositories/prisma-project-repository.ts
+++ src/infra/database/prisma/repositories/prisma-project-repository.ts
@@ -3,6 +3,8 @@ import prisma from "../client/prisma";
 import Boom from "@hapi/boom";
 import { ProjectRepository } from "~/domain/repositories/ProjectRepository";
 import { PrismaProjectMapper } from "../mappers/prisma-project-mapper";
+import { User } from "~/domain/entities/User/User";
+import { PrismaUserMapper } from "../mappers/prisma-user-mapper";
 
 export class PrismaProjectRepository implements ProjectRepository {
   async findById(id: string): Promise<Project | null> {
@@ -46,6 +48,36 @@ export class PrismaProjectRepository implements ProjectRepository {
       if (err.code === "P2023") {
         throw Boom.badRequest("ID Invalid");
       }
+      throw Boom.badRequest(err.message);
+    }
+  }
+
+  async findAllParticipantsByProjectId(projectId: string): Promise<User[]> {
+    const participants = await prisma.projectParticipanting.findMany({
+      where: { projectId: projectId },
+      include: {
+        user: true,
+      },
+    });
+
+    const users = participants.map((participant) => participant.user);
+
+    return users.map((user) => PrismaUserMapper.toDomain(user));
+  }
+
+  async addParticipantsInProject(
+    userId: string,
+    projectId: string,
+  ): Promise<void> {
+    try {
+      await prisma.projectParticipanting.create({
+        data: { userId, projectId },
+      });
+    } catch (err: any) {
+      if (err.code === "P2023") {
+        throw Boom.badRequest("ID's Inválidos");
+      }
+
       throw Boom.badRequest(err.message);
     }
   }
