9fa0da0f1d3957e00b6c4edea09bef03dcb96e2a
Jackson Silva Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-05-22 22:54:27 -0300

    feat: add fint task by id route

--- src/infra/http/routes/tasks.routes.ts
+++ src/infra/http/routes/tasks.routes.ts
@@ -2,12 +2,18 @@ import Hapi from "@hapi/hapi";
 import {
   addTaskResponsibleController,
   createTaskController,
+  findTaskByIdController,
 } from "../controllers/Task";
 
 const tasksRoutes = {
   name: "tasks",
   register: async function (server: Hapi.Server) {
     server.route([
+      {
+        method: "GET",
+        path: "/v1/tasks/{id}/",
+        handler: findTaskByIdController.handler,
+      },
       {
         method: "POST",
         path: "/v1/tasks/",
