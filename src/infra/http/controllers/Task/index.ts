b16bc0c355e666c54ee4895714dccd43423880d6
Jackson Silva Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-05-22 21:58:15 -0300

    feat: export add task responsible and inject dependency

--- src/infra/http/controllers/Task/index.ts
+++ src/infra/http/controllers/Task/index.ts
@@ -1,11 +1,17 @@
 import { CreateTaskCase } from "~/application/use-cases/tasks/create-task-case";
 import { PrismaTaskRepository } from "~/infra/database/prisma/repositories/prisma-task-repository";
 import { CreateTaskController } from "./CreateTaskController";
+import { AddTaskResponsibleController } from "./AddTaskResponsibleController";
+import { AddTaskResponsibleCase } from "~/application/use-cases/tasks/add-task-responsible-case";
 
 const prismaTaskRepository = new PrismaTaskRepository();
 
 const createTaskCase = new CreateTaskCase(prismaTaskRepository);
+const addTaskResponsibleCase = new AddTaskResponsibleCase(prismaTaskRepository);
 
 const createTaskController = new CreateTaskController(createTaskCase);
+const addTaskResponsibleController = new AddTaskResponsibleController(
+  addTaskResponsibleCase,
+);
 
-export { createTaskController };
+export { createTaskController, addTaskResponsibleController };
