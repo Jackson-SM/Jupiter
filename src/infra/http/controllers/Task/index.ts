85fb2bae655108de2104e434adc73d3c50c4a025
Jackson Silva Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-05-22 22:23:36 -0300

    feat: add export find task by responsible and inject dependencies

--- src/infra/http/controllers/Task/index.ts
+++ src/infra/http/controllers/Task/index.ts
@@ -3,15 +3,29 @@ import { PrismaTaskRepository } from "~/infra/database/prisma/repositories/prism
 import { CreateTaskController } from "./CreateTaskController";
 import { AddTaskResponsibleController } from "./AddTaskResponsibleController";
 import { AddTaskResponsibleCase } from "~/application/use-cases/tasks/add-task-responsible-case";
+import { FindTaskByResponsibleIdController } from "./FindTaskByResponsibleIdController";
+import { FindTaskByResponsibleIdCase } from "~/application/use-cases/tasks/find-task-by-responsible-id-case";
 
 const prismaTaskRepository = new PrismaTaskRepository();
 
+// Cases
 const createTaskCase = new CreateTaskCase(prismaTaskRepository);
 const addTaskResponsibleCase = new AddTaskResponsibleCase(prismaTaskRepository);
+const findTaskByResponsibleIdCase = new FindTaskByResponsibleIdCase(
+  prismaTaskRepository,
+);
 
+// Controllers
 const createTaskController = new CreateTaskController(createTaskCase);
 const addTaskResponsibleController = new AddTaskResponsibleController(
   addTaskResponsibleCase,
 );
+const findTaskByResponsibleIdController = new FindTaskByResponsibleIdController(
+  findTaskByResponsibleIdCase,
+);
 
-export { createTaskController, addTaskResponsibleController };
+export {
+  createTaskController,
+  addTaskResponsibleController,
+  findTaskByResponsibleIdController,
+};
