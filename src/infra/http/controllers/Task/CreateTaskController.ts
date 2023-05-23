0315035e9f329f02c70053b6dc7e5cbf32ac5cc8
Jackson Silva Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-05-22 21:00:40 -0300

    fix: fixed format returned in function

--- src/infra/http/controllers/Task/CreateTaskController.ts
+++ src/infra/http/controllers/Task/CreateTaskController.ts
@@ -1,6 +1,7 @@
 import { Request, ResponseToolkit } from "@hapi/hapi";
 import { CreateTaskCase } from "~/application/use-cases/tasks/create-task-case";
 import { CreateTaskBody } from "../../dtos/create-task-body";
+import { TaskViewModel } from "../../view-models/task-view-model";
 
 export class CreateTaskController {
   constructor(private createTaskCase: CreateTaskCase) {}
@@ -8,12 +9,14 @@ export class CreateTaskController {
   public handler = async (request: Request, h: ResponseToolkit) => {
     const { title, description, projectId } = request.payload as CreateTaskBody;
 
-    const task = await this.createTaskCase.execute({
+    const { task } = await this.createTaskCase.execute({
       title,
       description,
       projectId,
     });
 
-    return h.response(task).code(201);
+    const taskFormatHttp = TaskViewModel.toHttp(task);
+
+    return h.response(taskFormatHttp).code(201);
   };
 }
