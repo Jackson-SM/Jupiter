59570893daad5944baf001c9d633a9aa45e2da0e
Jackson Silva Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-05-24 23:53:17 -0300

    feat: add create comment controller

---
+++ src/infra/http/controllers/Comment/CreateCommentController.ts
@@ -0,0 +1,20 @@
+import { Request, ResponseToolkit } from "@hapi/hapi";
+import { CreateCommentCase } from "~/application/use-cases/comments/create-comment-case";
+import { CreateCommentBody } from "../../dtos/create-comment-body";
+import { CommentViewModel } from "../../view-models/comment-view-model";
+
+export class CreateCommentController {
+  constructor(private createCommentCase: CreateCommentCase) {}
+
+  public handler = async (request: Request, h: ResponseToolkit) => {
+    const { content, taskId, userId } = request.payload as CreateCommentBody;
+
+    const { comment } = await this.createCommentCase.execute({
+      content,
+      taskId,
+      userId,
+    });
+
+    return CommentViewModel.toHttp(comment);
+  };
+}
