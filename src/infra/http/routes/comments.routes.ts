41f07eac6adc14c4d9349ab5f182c088d187449e
Jackson Silva Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-05-25 00:01:18 -0300

    feat: add route create comment

--- src/infra/http/routes/comments.routes.ts
+++ src/infra/http/routes/comments.routes.ts
@@ -1,15 +1,14 @@
 import Hapi, { Request, ResponseToolkit } from "@hapi/hapi";
+import { createCommentController } from "../controllers/Comment";
 
 const commentsRoutes = {
   name: "comments",
   register: async function (server: Hapi.Server) {
     server.route([
       {
-        method: "GET",
+        method: "POST",
         path: "/v1/comments/",
-        handler: function (request: Request, h: ResponseToolkit) {
-          return "Hello World";
-        },
+        handler: createCommentController.handler,
       },
     ]);
   },
