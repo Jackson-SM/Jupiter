5a4544908eaf93e0c615d9d4d4a1462f49fd5f06
Jackson Silva Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-05-25 03:44:20 -0300

    feat: add find comment by id controller

---
+++ src/infra/http/controllers/Comment/FindCommentByIdController.ts
@@ -0,0 +1,15 @@
+import { Request, ResponseToolkit } from "@hapi/hapi";
+import { FindCommentByIdCase } from "~/application/use-cases/comments/find-comment-by-id-case";
+import { CommentViewModel } from "../../view-models/comment-view-model";
+
+export class FindCommentByIdController {
+  constructor(private findCommentByIdCase: FindCommentByIdCase) {}
+
+  public handler = async (request: Request, h: ResponseToolkit) => {
+    const { id } = request.query;
+
+    const { comment } = await this.findCommentByIdCase.execute({ id: id });
+
+    return CommentViewModel.toHttp(comment);
+  };
+}
