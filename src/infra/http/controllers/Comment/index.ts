b6498dc89d4681a55da9aaf6a4d0420baed0bebd
Jackson Silva Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-05-25 03:44:32 -0300

    feat: export all controllers and injection dependency

--- src/infra/http/controllers/Comment/index.ts
+++ src/infra/http/controllers/Comment/index.ts
@@ -1,13 +1,40 @@
 import { CreateCommentCase } from "~/application/use-cases/comments/create-comment-case";
 import { PrismaCommentRepository } from "~/infra/database/prisma/repositories/prisma-comment-repository";
 import { CreateCommentController } from "./CreateCommentController";
+import { FindCommentByIdCase } from "~/application/use-cases/comments/find-comment-by-id-case";
+import { FindCommentByIdController } from "./FindCommentByIdController";
+import { FindAllCommentsByTaskIdController } from "./FindAllCommentsByTaskIdController";
+import { FindAllCommentsByTaskIdCase } from "~/application/use-cases/comments/find-all-comments-by-task-id-case";
+import { FindAllCommentsByUserIdController } from "./FindAllCommentsByUserIdController";
+import { FindAllCommentsByUserIdCase } from "~/application/use-cases/comments/find-all-comments-by-user-id-case";
 
 const prismaCommentRepository = new PrismaCommentRepository();
 
 // Use Cases
 const createCommentCase = new CreateCommentCase(prismaCommentRepository);
+const findCommentByIdCase = new FindCommentByIdCase(prismaCommentRepository);
+const findAllCommentsByTaskIdCase = new FindAllCommentsByTaskIdCase(
+  prismaCommentRepository,
+);
+const findAllCommentsByUserIdCase = new FindAllCommentsByUserIdCase(
+  prismaCommentRepository,
+);
 
 // Controller
 const createCommentController = new CreateCommentController(createCommentCase);
+const findCommentByIdController = new FindCommentByIdController(
+  findCommentByIdCase,
+);
+const findAllCommentsByTaskIdController = new FindAllCommentsByTaskIdController(
+  findAllCommentsByTaskIdCase,
+);
+const findAllCommentsByUserIdController = new FindAllCommentsByUserIdController(
+  findAllCommentsByUserIdCase,
+);
 
-export { createCommentController };
+export {
+  createCommentController,
+  findCommentByIdController,
+  findAllCommentsByTaskIdController,
+  findAllCommentsByUserIdController,
+};
