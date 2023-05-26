d23fba516011aa7cfc90767cd187ae19aa30afcf
Jackson Silva Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-05-26 12:16:56 -0300

    feat: add remove task

--- src/domain/repositories/TaskRepository.ts
+++ src/domain/repositories/TaskRepository.ts
@@ -7,4 +7,5 @@ export interface TaskRepository {
   findByProjectId(projectId: string): Promise<Task[]>;
   findByResponsibleId(responsibleId: string): Promise<Task[]>;
   addResponsible(userId: string, taskId: string): Promise<void>;
+  removeTask(taskId: string): Promise<void>;
 }
