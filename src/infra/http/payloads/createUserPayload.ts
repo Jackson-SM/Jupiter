ac6ab27aec0732637b6e5036ca6202da423be174
Jackson Silva Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-05-26 01:10:29 -0300

    refactor: create user payload refactor

--- src/infra/http/payloads/createUserPayload.ts
+++ src/infra/http/payloads/createUserPayload.ts
@@ -2,8 +2,8 @@ import Joi from "@hapi/joi";
 import { CreateUserBody } from "../dtos/create-user-body";
 
 export default Joi.object<CreateUserBody>({
-  firstName: Joi.string().required(),
-  lastName: Joi.string().required(),
+  firstName: Joi.string().min(1).max(50).required(),
+  lastName: Joi.string().min(1).max(50).required(),
   email: Joi.string().email().required(),
-  password: Joi.string().required(),
+  password: Joi.string().min(1).max(20).required(),
 });
