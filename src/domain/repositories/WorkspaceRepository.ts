30cba536452a532ef30f02b227959547c32cbdbf
Jackson Silva Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-05-26 12:17:03 -0300

    feat: add remove workspace

--- src/domain/repositories/WorkspaceRepository.ts
+++ src/domain/repositories/WorkspaceRepository.ts
@@ -4,4 +4,5 @@ export interface WorkspaceRepository {
   create(workspace: Workspace): Promise<void>;
   findByid(id: string): Promise<Workspace | null>;
   getAllByCreatorId(creatorId: string): Promise<Workspace[]>;
+  removeWorkspace(workspaceId: string): Promise<void>;
 }
