90e017dfc964922c6a99c22cb7079db8986e13fb
Jackson Silva Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-05-26 12:16:27 -0300

    feat: add remove project

--- src/domain/repositories/ProjectRepository.ts
+++ src/domain/repositories/ProjectRepository.ts
@@ -8,4 +8,5 @@ export interface ProjectRepository {
   findAllByWorkspaceId(workspaceId: string): Promise<Project[]>;
   findAllParticipantsByProjectId(projectId: string): Promise<User[]>;
   addParticipantsInProject(userId: string, projectId: string): Promise<void>;
+  removeProject(projectId: string): Promise<void>;
 }
