0b728a9f134e40aac52f9e2855e4a8205f9d9e08
Jackson Silva Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-05-28 03:41:21 -0300

    feat: add edit group case

---
+++ src/application/use-cases/groups/edit-group-case.ts
@@ -0,0 +1,16 @@
+import { GroupRepository } from "~/domain/repositories/GroupRepository";
+
+interface CreateGroupCaseRequest {
+  id: string;
+  name: string;
+}
+
+export class EditGroupCase {
+  constructor(private groupRepository: GroupRepository) {}
+
+  async execute(request: CreateGroupCaseRequest): Promise<void> {
+    const { id, name } = request;
+
+    await this.groupRepository.editNameGroup(id, name);
+  }
+}
