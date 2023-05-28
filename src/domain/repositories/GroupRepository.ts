c615fef9f8e047ac30cb7b0f29c8c4d1b6b7eb14
Jackson Silva Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-05-28 03:41:41 -0300

    fix: fixed params in edit group case

--- src/domain/repositories/GroupRepository.ts
+++ src/domain/repositories/GroupRepository.ts
@@ -3,7 +3,7 @@ import { Group } from "../entities/Group/Group";
 export interface GroupRepository {
   createGroup(group: Group): Promise<void>;
   deleteGroup(groupId: string): Promise<void>;
-  editGroup(groupId: string, group: Group): Promise<void>;
+  editNameGroup(groupId: string, name: string): Promise<void>;
   addTaskInGroup(taskId: string, groupId: string): Promise<void>;
   moveTaskGroup(newGroup: string, taskId: string): Promise<void>;
   removeTaskInGroup(taskId: string, groupId: string): Promise<void>;
