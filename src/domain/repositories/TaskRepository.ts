3bc4864bcb4e971cbd0bf85ac57bc2aff41f864f
Jackson Silva Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-05-28 03:30:32 -0300

    fix: remove find task by project id

--- src/domain/repositories/TaskRepository.ts
+++ src/domain/repositories/TaskRepository.ts
@@ -3,7 +3,6 @@ import { Task } from "../entities/Task/Task";
 export interface TaskRepository {
   create(task: Task): Promise<void>;
   findById(id: string): Promise<Task | null>;
-  findByProjectId(projectId: string): Promise<Task[]>;
   findByResponsibleId(responsibleId: string): Promise<Task[]>;
   findTasksByGroupId(groupId: string): Promise<Task[]>;
   addResponsible(userId: string, taskId: string): Promise<void>;
