7452aa3e914f2acab134b05b39a6db41fe1dfd90
Jackson Silva Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-05-27 22:55:04 -0300

    fix: add groupId in makeTask

--- tests/factories/makeTask.ts
+++ tests/factories/makeTask.ts
@@ -1,4 +1,5 @@
 import { Task } from "../../src/domain/entities/Task/Task";
+import { makeGroup } from "./makeGroup";
 import { makeUser } from "./makeUser";
 
 type Override = Partial<Task>;
@@ -7,6 +8,7 @@ export function makeTask(override?: Override): Task {
   return new Task({
     title: "new task",
     description: "description of the task",
+    groupId: makeGroup().id,
     projectId: makeUser().id,
     ...override,
   });
