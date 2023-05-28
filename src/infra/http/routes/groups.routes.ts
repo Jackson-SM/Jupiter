6a0f3753f1512f0307a2c232fd1cb1df8444fdea
Jackson Silva Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-05-28 13:09:13 -0300

    feat: add find task by group id

---
+++ src/infra/http/routes/groups.routes.ts
@@ -0,0 +1,23 @@
+import Hapi from "@hapi/hapi";
+import payloadParamsId from "../payloads/payloadParamsId";
+import { findTaskByGroupIdController } from "../controllers/Task";
+
+const commentsRoutes = {
+  name: "groups",
+  register: async function (server: Hapi.Server) {
+    server.route([
+      {
+        method: "GET",
+        path: "/v1/groups/{id}/tasks/",
+        handler: findTaskByGroupIdController.handler,
+        options: {
+          validate: {
+            params: payloadParamsId,
+          },
+        },
+      },
+    ]);
+  },
+};
+
+export default commentsRoutes;
