9ad8a511f1a920f3d0ce63b784fb3debecb74488
Jackson Silva Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-05-27 22:53:33 -0300

    feat: add tasksInGroup relation entity

---
+++ src/domain/entities/TasksInGroup/TasksInGroup.ts
@@ -0,0 +1,58 @@
+import { Replace } from "../../../helpers/Replace";
+import ObjectID from "bson-objectid";
+
+interface TaskResponsibleProps {
+  groupId: string;
+  taskId: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export class TasksInGroup {
+  private _id: string;
+  private props: TaskResponsibleProps;
+
+  constructor(
+    props: Replace<
+      TaskResponsibleProps,
+      { createdAt?: Date; updatedAt?: Date }
+    >,
+    _id?: string,
+  ) {
+    this._id = _id ?? new ObjectID().toHexString();
+    this.props = {
+      ...props,
+      createdAt: props.createdAt ?? new Date(),
+      updatedAt: new Date(),
+    };
+  }
+
+  get id(): string {
+    return this._id;
+  }
+
+  get groupId(): string {
+    return this.props.groupId;
+  }
+  set groupId(groupId: string) {
+    this.props.groupId = groupId;
+  }
+
+  get taskId(): string {
+    return this.props.taskId;
+  }
+  set taskId(taskId: string) {
+    this.props.taskId = taskId;
+  }
+
+  get createdAt(): Date {
+    return this.props.createdAt;
+  }
+
+  get updatedAt(): Date {
+    return this.props.updatedAt;
+  }
+  set updatedAt(updatedAt: Date) {
+    this.props.updatedAt = updatedAt;
+  }
+}
