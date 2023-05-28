08c04b97451f549e1618c5a163c6761d9b488d91
Jackson Silva Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-05-28 19:48:39 -0300

    feat: import routes groups and auth

--- src/infra/http/routes/routes.ts
+++ src/infra/http/routes/routes.ts
@@ -3,5 +3,7 @@ import workspaces from "./workspace.routes";
 import projects from "./projects.routes";
 import tasks from "./tasks.routes";
 import comments from "./comments.routes";
+import groups from "./groups.routes";
+import auth from "./auth.routes";
 
-export { users, workspaces, projects, tasks, comments };
+export { users, workspaces, projects, tasks, comments, groups, auth };
