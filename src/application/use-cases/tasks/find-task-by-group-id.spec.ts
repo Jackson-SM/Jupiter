4bc2c82e0a086e7a63014406009e5a228cfdb1bb
Jackson Silva Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-05-27 23:31:08 -0300

    test: add find task by group id testing

---
+++ src/application/use-cases/tasks/find-task-by-group-id.spec.ts
@@ -0,0 +1,39 @@
+import { CreateTaskCase } from "./create-task-case";
+import { InMemoryTaskRepository } from "tests/repositories/in-memory-task-repository";
+import { makeTask } from "tests/factories/makeTask";
+import { Task } from "~/domain/entities/Task/Task";
+import { FindTaskByGroupIdCase } from "./find-task-by-group-id";
+import { Group } from "~/domain/entities/Group/Group";
+import { makeGroup } from "tests/factories/makeGroup";
+
+describe("Find Task By Group Id Case", () => {
+  let inMemoryTaskRepository: InMemoryTaskRepository;
+  let createTaskCase: CreateTaskCase;
+  let findTaskByGroupIdCase: FindTaskByGroupIdCase;
+  let taskTesting: Task;
+  let groupTesting: Group;
+
+  beforeEach(async () => {
+    inMemoryTaskRepository = new InMemoryTaskRepository();
+    createTaskCase = new CreateTaskCase(inMemoryTaskRepository);
+    findTaskByGroupIdCase = new FindTaskByGroupIdCase(inMemoryTaskRepository);
+    groupTesting = makeGroup();
+    const { task } = await createTaskCase.execute(
+      makeTask({ groupId: groupTesting.id }),
+    );
+    taskTesting = task;
+  });
+
+  it("should to find a Task with group id equal group testing", async () => {
+    const { tasks } = await findTaskByGroupIdCase.execute({
+      id: groupTesting.id,
+    });
+
+    expect(tasks).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({ groupId: groupTesting.id }),
+      ]),
+    );
+    expect(tasks[0].groupId).toEqual(groupTesting.id);
+  });
+});
