c862ef34c3c3c1bfbbe7c36652a830e7e3cb259c
Jackson Silva Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-05-28 04:10:06 -0300

    test: testing add task in group case

---
+++ src/application/use-cases/groups/add-task-in-group-case.spec.ts
@@ -0,0 +1,38 @@
+import { CreateGroupCase } from "./create-group-case";
+import { InMemoryGroupRepository } from "tests/repositories/in-memory-group-repository";
+import { makeGroup } from "tests/factories/makeGroup";
+import { Group } from "~/domain/entities/Group/Group";
+import { EditNameGroupCase } from "./edit-name-group-case";
+import { AddTaskInGroupCase } from "./add-task-in-group-case";
+import { Task } from "~/domain/entities/Task/Task";
+import { makeTask } from "tests/factories/makeTask";
+
+describe("Add Task In Group Case", () => {
+  let inMemoryGroupRepository: InMemoryGroupRepository;
+  let createGroupCase: CreateGroupCase;
+  let groupTesting: Group;
+  let taskTesting: Task;
+  let addTaskInGroupCase: AddTaskInGroupCase;
+
+  beforeEach(async () => {
+    inMemoryGroupRepository = new InMemoryGroupRepository();
+    createGroupCase = new CreateGroupCase(inMemoryGroupRepository);
+    addTaskInGroupCase = new AddTaskInGroupCase(inMemoryGroupRepository);
+    const { group } = await createGroupCase.execute(makeGroup());
+    groupTesting = group;
+    taskTesting = makeTask();
+  });
+
+  it("should add a task in group", async () => {
+    await addTaskInGroupCase.execute({
+      groupId: groupTesting.id,
+      taskId: taskTesting.id,
+    });
+
+    expect(groupTesting).toBeInstanceOf(Group);
+    expect(inMemoryGroupRepository.tasksInGroup).toHaveLength(1);
+    expect(inMemoryGroupRepository.tasksInGroup[0].taskId).toEqual(
+      taskTesting.id,
+    );
+  });
+});
