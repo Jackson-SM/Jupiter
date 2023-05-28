35b85409b5e66905085efa0841c02134c9949f8d
Jackson Silva Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-05-27 22:52:41 -0300

    feat: add group entity

---
+++ src/domain/entities/Group/Group.ts
@@ -0,0 +1,48 @@
+import ObjectID from "bson-objectid";
+import { Replace } from "~/helpers/Replace";
+
+interface GroupProps {
+  name: string;
+  projectId: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export class Group {
+  private _id: string;
+  private props: GroupProps;
+
+  constructor(
+    props: Replace<GroupProps, { createdAt?: Date; updatedAt?: Date }>,
+    _id?: string,
+  ) {
+    this._id = _id ?? new ObjectID().toHexString();
+    this.props = {
+      ...props,
+      createdAt: props.createdAt ?? new Date(),
+      updatedAt: new Date(),
+    };
+  }
+
+  get id() {
+    return this._id;
+  }
+
+  get name(): string {
+    return this.props.name;
+  }
+  set name(name: string) {
+    this.props.name = name;
+  }
+
+  get createdAt(): Date {
+    return this.props.createdAt;
+  }
+
+  get updatedAt(): Date {
+    return this.props.updatedAt;
+  }
+  set updatedAt(updatedAt: Date) {
+    this.props.updatedAt = updatedAt;
+  }
+}
