ae5550ae836dbff336ab62646c56246caa505eaf
Jackson Silva Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-06-01 00:43:59 -0300

    fix: fixed payload create project

--- src/infra/http/payloads/createProjectPayload.ts
+++ src/infra/http/payloads/createProjectPayload.ts
@@ -3,7 +3,7 @@ import { ICreateProjectBody } from "../dtos/create-project-body";
 
 export default Joi.object<ICreateProjectBody>({
   title: Joi.string().min(3).max(20).required(),
-  description: Joi.string().min(3).max(20).required(),
+  description: Joi.string().min(3).max(200).required(),
   leadId: Joi.string().length(24).required(),
   workspaceId: Joi.string().length(24).required(),
 });
