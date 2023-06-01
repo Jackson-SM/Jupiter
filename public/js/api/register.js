86b2becff7923b17e8b726241a988f1cc3072eac
Jackson Silva Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-06-01 19:02:48 -0300

    feat: add register function

---
+++ public/js/api/register.js
@@ -0,0 +1,35 @@
+const formRegister = document.querySelector('.form-register')
+
+async function handleSubmit(event) {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+  const formObject = Object.fromEntries(formData.entries()); // Transformando o FormData em Objeto
+  console.log(formObject);
+
+  const data = register({firstName: formObject.firstName, lastName: formObject.lastName, email: formObject.email, password: formObject.password})
+}
+
+const register = async (data) => {
+    const response = await fetch("http://localhost:3000/v1/users/", {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+
+  const responseData = await response.json()
+
+  if(responseData.statusCode) {
+    return createNotification("Alguma informação está incorreta.")
+  }
+
+  setCookie('token', responseData.token, 2);
+  sessionStorage.setItem('user', JSON.stringify(responseData.user));
+  window.location.href = "/";
+
+  return responseData.user;
+}
+
+formRegister.addEventListener('submit', handleSubmit);
