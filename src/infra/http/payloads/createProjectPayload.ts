2296c95fa27bec57ca492836f0cfaecd90b27353
Jackson Silva Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-06-01 00:47:18 -0300

    fix: fixed title max

--- src/infra/http/payloads/createProjectPayload.ts
+++ src/infra/http/payloads/createProjectPayload.ts
@@ -2,7 +2,7 @@ import Joi from "@hapi/joi";
 import { ICreateProjectBody } from "../dtos/create-project-body";
 
 export default Joi.object<ICreateProjectBody>({
-  title: Joi.string().min(3).max(20).required(),
+  title: Joi.string().min(3).max(30).required(),
   description: Joi.string().min(3).max(200).required(),
   leadId: Joi.string().length(24).required(),
   workspaceId: Joi.string().length(24).required(),
