386052ea5fbb38b3ea8a739474a3f06af69eb448
Jackson Silva Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-05-31 23:40:27 -0300

    feat: add get cookie token

--- public/js/auth/verify-authenticate.js
+++ public/js/auth/verify-authenticate.js
@@ -1,9 +1,18 @@
-const token = localStorage.getItem('token')
-
-console.log(token)
-
-document.addEventListener("DOMContentLoaded", function() {
-  if(token) {
-   return window.location.href = "http://localhost:3001/"
+function getCookie(name) {
+  const cookieName = name + '=';
+  const decodedCookie = decodeURIComponent(document.cookie);
+  const cookieArray = decodedCookie.split(';');
+  
+  for (let i = 0; i < cookieArray.length; i++) {
+    let cookie = cookieArray[i];
+    while (cookie.charAt(0) === ' ') {
+      cookie = cookie.substring(1);
+    }
+    if (cookie.indexOf(cookieName) === 0) {
+      return cookie.substring(cookieName.length, cookie.length);
+    }
   }
-});
+  return '';
+}
+
+console.log(getCookie('token'));
