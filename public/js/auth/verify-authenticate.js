9027a9031677728405813235fdae336a9b2c3fa4
Jackson Silva Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-06-01 19:02:40 -0300

    feat: add verification login and token

--- public/js/auth/verify-authenticate.js
+++ public/js/auth/verify-authenticate.js
@@ -1,18 +1,3 @@
-function getCookie(name) {
-  const cookieName = name + '=';
-  const decodedCookie = decodeURIComponent(document.cookie);
-  const cookieArray = decodedCookie.split(';');
-  
-  for (let i = 0; i < cookieArray.length; i++) {
-    let cookie = cookieArray[i];
-    while (cookie.charAt(0) === ' ') {
-      cookie = cookie.substring(1);
-    }
-    if (cookie.indexOf(cookieName) === 0) {
-      return cookie.substring(cookieName.length, cookie.length);
-    }
-  }
-  return '';
+if(getCookie('token').length < 1 || !sessionStorage.getItem('user')) {
+  logout()
 }
-
-console.log(getCookie('token'));
