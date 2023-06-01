c3cad6050cab83e2f9d7bc1e6d547693b7759f0e
Jackson Silva Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-06-01 00:34:15 -0300

    feat: login

--- public/js/api/login.js
+++ public/js/api/login.js
@@ -1,10 +1,37 @@
 const formLogin = document.querySelector('.form-login')
 
+function setCookie(name, value, days) {
+  let expires = '';
+  if (days) {
+    const date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    expires = '; expires=' + date.toUTCString();
+  }
+  document.cookie = name + '=' + (value || '') + expires + '; Secure; path=/';
+}
+
+function getCookie(name) {
+  const cookieName = name + '=';
+  const decodedCookie = decodeURIComponent(document.cookie);
+  const cookieArray = decodedCookie.split(';');
+  
+  for (let i = 0; i < cookieArray.length; i++) {
+    let cookie = cookieArray[i];
+    while (cookie.charAt(0) === ' ') {
+      cookie = cookie.substring(1);
+    }
+    if (cookie.indexOf(cookieName) === 0) {
+      return cookie.substring(cookieName.length, cookie.length);
+    }
+  }
+  return '';
+}
+
 async function handleSubmit(event) {
   event.preventDefault();
 
   const formData = new FormData(event.target);
-  const formObject = Object.fromEntries(formData.entries());
+  const formObject = Object.fromEntries(formData.entries()); // Transformando o FormData em Objeto
   console.log(formObject);
 
   if(formObject.password !== formObject.confirmPassword) {
@@ -31,8 +58,9 @@ const login = async (data) => {
     return createNotification("Conta não encontrada.")
   }
 
-  localStorage.setItem('token', JSON.stringify(responseData.token))
-  localStorage.setItem('user', JSON.stringify(responseData.user))
+  setCookie('token', responseData.token, 2);
+
+  sessionStorage.setItem('user', JSON.stringify(responseData.user))
 
   return responseData.user;
 }
