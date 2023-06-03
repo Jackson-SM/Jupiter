19cba9303eafc6ca8634593fa93166f90a3bff70
Jackson Silva Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-06-03 02:51:29 -0300

    feat: add user dropdown and workspace

--- public/js/conditional-rendering/rendering-workspace.js
+++ public/js/conditional-rendering/rendering-workspace.js
@@ -7,23 +7,39 @@ async function renderWorkspaces() {
     const workspace = document.createElement('div');
     workspace.classList.add('workspace');
     
+    const title_workspace = document.createElement('h3');
     const top_workspace = document.createElement('div');
     top_workspace.classList.add("top_workspace");
-    const title_workspace = document.createElement('h3');
-    title_workspace.textContent = data.title;
+    title_workspace.innerHTML = `${data.title}<i class="gg-arrow-down"></i>`;
+    top_workspace.appendChild(title_workspace);
+    workspace.appendChild(top_workspace) // Add In Workspace
 
-    const projects_workspace = document.createElement('div')
-    projects_workspace.classList.add("projects_workspace")
-    top_workspace.appendChild(projects_workspace)
+    const projects_workspace = document.createElement('div');
+    projects_workspace.classList.add("projects_workspace");
+    workspace.appendChild(projects_workspace); // Add Collection Projects
 
     data.projects.map((project) => {
       const projectElement = document.createElement('a')
       projectElement.classList.add("project")
       projectElement.setAttribute("href", `project?id=${project.id}`)
       projects_workspace.appendChild(projectElement)
+
+      // Content Project
+      const content_project = document.createElement('div')
+      content_project.classList.add("content_project")
+      const title_project = document.createElement('span')
+      title_project.classList.add("title")
+      title_project.textContent = project.title
+      
+      content_project.appendChild(title_project)
+
+      projectElement.appendChild(content_project)
     })
 
-    console.log(data)
+    const create_project = document.createElement('div')
+    create_project.classList.add("project", "create_project")
+    create_project.innerHTML = '<i class="gg-add-r"></i> Criar Projeto'
+    projects_workspace.appendChild(create_project)
 
     section_workspaces.appendChild(workspace);
   })
--- public/js/conditional-rendering/user-options-conditional.js
+++ public/js/conditional-rendering/user-options-conditional.js
@@ -1,13 +1,15 @@
 const user_options = document.querySelector('.user-options');
-console.log(user_options)
+const dropdown_user = document.querySelector('.dropdown_user')
 
 function execConditional() {
   if(getCookie('token').length > 0) {
     const button_user_options = document.createElement('button')
     button_user_options.classList.add("user_dropdown")
-    button_user_options.innerHTML = `${user.firstName}<i class="gg-more-vertical-alt"></i>`
+    button_user_options.innerHTML = `${user.firstName}<i class="gg-user-list"></i>`
 
-    console.log(button_user_options)
+    button_user_options.addEventListener('click', () => {
+      dropdown_user.classList.toggle('open')
+    })
 
     return user_options.appendChild(button_user_options);
   }
