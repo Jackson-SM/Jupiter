d111701f9d16c2ecc12412ebe07d3ecda2dec487
Jackson Silva Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-06-03 16:58:10 -0300

    fix: remove try catch login

--- src/infra/http/controllers/Authentication/AuthenticationLoginController.ts
+++ src/infra/http/controllers/Authentication/AuthenticationLoginController.ts
@@ -9,19 +9,13 @@ export class AuthenticationLoginController {
   public handler = async (request: Request, h: ResponseToolkit) => {
     const { email, password } = request.payload as IAuthenticateBody;
 
-    try {
-      const { token, user } = await this.authencationUseCase.authenticate(
-        email,
-        password,
-      );
+    const { token, user } = await this.authencationUseCase.authenticate(
+      email,
+      password,
+    );
 
-      const userFormatToHttp = UserViewModel.toHttp(user);
+    const userFormatToHttp = UserViewModel.toHttp(user);
 
-      return h.response({ user: userFormatToHttp, token: token }).code(200);
-    } catch (err: any) {
-      return h
-        .response({ message: err.message, statusCode: err.output.statusCode })
-        .code(err.output.statusCode);
-    }
+    return h.response({ user: userFormatToHttp, token: token }).code(200);
   };
 }
