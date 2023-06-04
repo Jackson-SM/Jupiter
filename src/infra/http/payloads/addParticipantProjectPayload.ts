cb073b5a04fe1b5e7a329fffbf04c28e9df3601a
Jackson Silva Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-06-03 22:48:10 -0300

    fix: fixed add participant project payload

--- src/infra/http/payloads/addParticipantProjectPayload.ts
+++ src/infra/http/payloads/addParticipantProjectPayload.ts
@@ -4,4 +4,4 @@ import { AddParticipantInProjectBody } from "../dtos/add-participant-in-project-
 export default Joi.object<AddParticipantInProjectBody>({
   email: Joi.string().email().required(),
   projectId: Joi.string().length(24).required(),
-}).options({ messages: { "string.email": "O Email deve ser válido." } });
+});
