c10dc39b379ff6b2aac11e771cf45379c9aa7ed1
Jackson Silva Magalhães <jacksonsilvamagalhaes@hotmail.com>
2023-06-04 00:24:11 -0300

    fix: configurate fixed

--- public/js/conditional-rendering/user-options-conditional.js
+++ public/js/conditional-rendering/user-options-conditional.js
@@ -5,7 +5,7 @@ function execConditional() {
   if(getCookie('token').length > 0) {
     const button_user_options = document.createElement('button')
     button_user_options.classList.add("user_dropdown")
-    button_user_options.innerHTML = `${user.firstName}<i class="gg-user-list"></i>`
+    button_user_options.innerHTML = `<i class="gg-user-list"></i>${user.firstName}`
 
     button_user_options.addEventListener('click', () => {
       dropdown_user.classList.toggle('open')
